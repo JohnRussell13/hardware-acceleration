17ffdf657fc7a729752416a79891c30e956703bf
JohnRussell13 <aleksandar.g1999@gmail.com>
2022-04-18 20:15:01 +0200

    Fixed main

--- vp/main.cpp
+++ vp/main.cpp
@@ -4,17 +4,14 @@
 using namespace sc_core;
 using namespace tlm;
 
-int counter;
 sc_core::sc_time offset;
 sc_core::sc_time delay;
 int t;
 int sc_main(int argc, char* argv[]){
-	counter = 0;
     if(argc != 3){
         return 0;
     }
-  	Vp vp("VP", argv[1], argv[2]);
-	sc_start();
-
-   return 0;
+    Vp vp("VP", argv[1], argv[2]);
+    sc_start();
+    return 0;
 }
